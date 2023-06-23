d30aad057c16a4d1e02f9e98497c987c83e5c14b
L0X1128 <asutora1128@gmail.com>
2023-06-24 00:29:57 +0900

    5th commit

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-const tasks = [];
+const addTask = () => {
 
-const taskValue = document.getElementesById('add-task');
+    //console.log("works");
 
-taskValue.addEventListener('submit', (event) => {
+    // 追加するTODOの内容を取得
+    const addTaskTitle = document.getElementById("addTaskTitle").value;
 
-    event.preventDefault();
+    //TODOの内容を消去し次のTODOに備える
+    document.getElementById("addTaskTitle").value = "";
 
-    const nameValue = document.getElementesById('add-task-value').value;
+    //タスク一覧関連の関数を実行
+    if(addTaskTitle != ""){
+        createTaskList(addTaskTitle);
+    }
+};
 
-    if(!nameValue) return;
+// タスク完了時に消す関数
+const deleteCompTask = (target) => {
+    document.getElementById("task-list").removeChild(target);
+}
 
-    const task = {id: tasks.length+1, name:nameValue};
+// タスク一覧関連の関数
+const createTaskList = (text) =>{
+    // divの生成
+    const div = document.createElement("div");
 
-    console.log("登録しました")
-})
+    // liの生成とliのtextを設定
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // Task横の完了ボタンの生成、ボタン押下時のTask削除
+    const compButton = document.createElement("button");
+    compButton.innerText = "完了";
+    compButton.addEventListener("click", () => {
+        deleteCompTask(compButton.parentNode);
+    })
+
+    div.appendChild(li);
+    div.appendChild(compButton);
+
+    document.getElementById("task-list").appendChild(div);
+}
+
+document
+    .getElementById("add-task")
+    .addEventListener("click", addTask);
