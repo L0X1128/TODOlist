60a07cb73faa7718db49f5853487cd64a2a76d91
L0X1128 <asutora1128@gmail.com>
2023-06-27 21:02:45 +0900

    6th commit

--- script.js
+++ script.js
@@ -41,6 +41,34 @@ const createTaskList = (text) =>{
     document.getElementById("task-list").appendChild(div);
 }
 
+// 時間が一桁の時に頭に0を足す関数
+const setTimeTo2 = (time) => {
+    var ret;
+    if(time<10){
+        ret = "0" + time;
+    }else{
+        ret = time;
+    }
+    return ret;
+}
+
+// 日時を表示する関数
+const showClock = () => {
+    const nowTime = new Date();
+    const nowYear = nowTime.getFullYear();
+    const nowMonth = nowTime.getMonth();
+    const nowDay = nowTime.getDate();
+    const nowHour = setTimeTo2(nowTime.getHours());
+    const nowMinute = setTimeTo2(nowTime.getMinutes());
+    const nowSecond = setTimeTo2(nowTime.getSeconds());
+
+    const msg = "現在:" + nowYear + "/" + (nowMonth+1) + "/" + nowDay + " - " + nowHour + ":" + nowMinute + ":" + nowSecond;
+    document.getElementById("RealTimeClock").innerHTML = msg;
+}
+
+// 時間の更新を1000ms毎に行う
+setInterval('showClock()', 1000);
+
 document
     .getElementById("add-task")
     .addEventListener("click", addTask);
